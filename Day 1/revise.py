76bf02cb816eef3ee60500791094c6d643bc14ff
Samir Paudel <128166301+paudelsamir@users.noreply.github.com>
2024-06-11 23:43:18 +0545

    Basics

---
+++ Day 1/revise.py
@@ -0,0 +1,14 @@
+# print("Hello World")
+
+# input("What's your name")
+
+#This is a comment
+# print("This is code")
+
+
+# my_name = "Angela"
+# my_age = 12
+
+# my_age = 12
+# my_age += 4
+#my_age is now 16
