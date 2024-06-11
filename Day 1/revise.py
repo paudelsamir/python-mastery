088edbe37063e9a61adae5d7b17eaae416c909c2
Samir Paudel <128166301+paudelsamir@users.noreply.github.com>
2024-06-11 23:50:12 +0545

    Update Data Tyoes

--- Day 1/revise.py
+++ Day 1/revise.py
@@ -1,3 +1,4 @@
+#BASICS
 # print("Hello World")
 
 # input("What's your name")
@@ -12,3 +13,29 @@
 # my_age = 12
 # my_age += 4
 #my_age is now 16
+
+
+#DATA TYPES
+# my_number = 354      - INTEGERS
+
+# my_float = 3.14159       - FLOATING POINT NUMBER
+
+# my_string = "Hello"       -STRINGS
+
+# "Hello" + "Angela"        -STRING CONCATINATION
+# #becomes "HelloAngela"
+
+# speech = "She said: \"Hi\""       -ESCAPING THE STRING
+# print(speech)
+# #prints: She said: "Hi"
+
+# days = 365               - F-STRING
+# print(f"There are {days}
+# in a year")
+
+# n = 354                   -CONVERTING DATA TYPES
+# new_n = float(n)
+# print(new_n) #result 354.0
+
+# n = 3.14159           -CHECKING DATA TYPES
+# type(n) #result float
