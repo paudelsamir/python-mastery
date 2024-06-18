64e35c37a049ccc5c89d1ba5d6e82b373300bfc4
Samir Paudel <128166301+paudelsamir@users.noreply.github.com>
2024-06-18 16:21:12 +0545

    Hangman Project Completed

--- day 7-Hangman Project/hangman.py
+++ day 7-Hangman Project/hangman.py
@@ -1,43 +1,50 @@
 import random
+from hangman_words import word_list
+from ascii_art import stages, logo
 
-words = 'ant babboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole rat raven rhino shark sheep spider toad turkey turtle wolf wombat zebra'
-words_list =words.split(" ")
-# print(words_list)
-# print(words_list[5])
+print(logo)
+print("\n\n")
 
-random_word =random.choice(words_list) 
+random_word = random.choice(word_list)
 print(random_word)
 
-to_fill = ""
-for alphabets in random_word:
-    to_fill = to_fill + "_"
-print(to_fill)
+display = []
+word_length = len(random_word)
+for _ in range(word_length):
+    display += "_"
+# print(display)
 
-life = len(random_word)
-# guess_letter = input("Please guess the letter: ").lower()
-
-          
-
-
-
-
-
-
-
-
-for letter in to_fill:
-    print(f"Your Lives remaining is {life}")
-    guess_letter = input("Please Guess the letter: ").lower()
-    for letter in random_word:
+lives = 5
+end_of_game = False
+while not end_of_game:
+    guess_letter = input("Please guess the letter: ").lower()
+    for position in range(word_length):
+        letter = random_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess_letter}")
         if letter == guess_letter:
-            for dyaas in to_fill:
-                dyass = guess_letter
-            print(guess_letter)
-            to_fill = to_fill + guess_letter
-        else:
-            print("_")
-            life -= 1   
-            if life ==  0:
-                print(f"Game Over! Your Lives remaining is{life}")
+            display[position]= letter     
+                                   
     
 
+    print(f" ".join(display))
+    print(f"\nGuessed letter: {guess_letter}")       
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You won. Thank You for saving an innocent man from hanging.")    
+        
+    for position in range(word_length):
+        letter = random_word[position]
+        if letter == guess_letter:
+            display[position] = letter
+
+    if guess_letter not in random_word:
+        lives -=1
+        if lives > 0:
+            print("The letter You guessed is not in the word!! be careful.")
+        if lives == 0:
+            end_of_game = True
+            print("\nYou let a innocent man die!!")
+            print(f"The Word to be guessed is {random_word}")
+                           
+    print(stages[lives])
